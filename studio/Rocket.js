dcb7e171b8745db26426e8c1a9b7eab71d020482
FatedMarigold <Darianezell1@gmail.com>
2022-08-15 22:12:24 -0500

    done?

---
+++ studio/Rocket.js
@@ -0,0 +1,49 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sumMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumMass += items[i].massKg;
+        }
+        return sumMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var currentMass = 0;
+        currentMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return currentMass;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
